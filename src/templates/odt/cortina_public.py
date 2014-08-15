454c5c3fa2206788164b81a656892b219ec1e118
Brad Elliott <brad_e@hotmail.com>
2014-08-15 11:54:04 -0400

    Missed a change in the latest build

--- src/templates/odt/cortina_public.py
+++ src/templates/odt/cortina_public.py
@@ -1,18 +1,18 @@
 import string
 
-from templates.odt.custom_styles import *
+from templates.odt.odt_styles import *
+import templates.themes
 
 class custom_styles(styles):
     def __init__(self):
         styles.__init__(self)
         
-        #self.table_indent = 0.65
         #self.list_bullet_indent  = 0.2
         #self.list_bullet_base    = 2.5
+        self.standard_indent = 0.5
+        self.table_indent = 0.5
 
-        self.list_bullet_text_indent = 0.4
-
-        self.standard_indent = 0
+        self.colors = templates.themes.theme().get_colors("cortina")
 
     def custom_styles(self):
 
@@ -44,7 +44,7 @@ class custom_styles(styles):
             ${break_before} fo:keep-with-next="always">
     		<style:tab-stops/>
     	</style:paragraph-properties>
-    	<style:text-properties fo:color="#0063a5" style:font-name="Arial2" fo:font-family="Arial"
+    	<style:text-properties fo:color="${color_heading}" style:font-name="Arial2" fo:font-family="Arial"
             style:font-style-name="Bold" style:font-family-generic="swiss" style:font-pitch="variable"
             ${font_size} fo:font-weight="bold" style:font-name-asian="Times New Roman"
             style:font-family-asian="&apos;Times New Roman&apos;" style:font-family-generic-asian="roman"
@@ -56,13 +56,18 @@ class custom_styles(styles):
                 "break_before" : break_before,
                 "margin_bottom" : margin_bottom,
                 "margin_left"   : margin_left,
-                "font_size"     : font_size})
+                "font_size"     : font_size,
+                "color_heading" : self.colors["heading.%d" % heading].fg})
     
         list_styles = self.get_list_styles()
 
         # Table Styles
         table_styles = self.get_table_styles()
     
+        prototype_styles = self.get_prototype_styles()
+
+        source_code_styles = self.get_source_code_styles()
+
         custom_styles = string.Template('''
     <style:style style:name="shorte_standard" style:family="paragraph" style:parent-style-name="Standard">
       <style:paragraph-properties fo:margin-top="0.4cm" fo:margin-bottom="0.4cm" fo:margin-left="${standard_indent}cm"/>
@@ -75,11 +80,18 @@ class custom_styles(styles):
     
     <!-- List styles -->
     $list_styles
+
+    <!-- Prototype styles -->
+    $prototype_styles
+
+    $source_code_styles
     ''').substitute(
             {
          "heading_styles" : heading_styles,
          "list_styles" : list_styles,
          "table_styles" : table_styles,
-         "standard_indent" : self.standard_indent})
+         "prototype_styles" : prototype_styles,
+         "standard_indent" : self.standard_indent,
+         "source_code_styles" : source_code_styles})
     
         return custom_styles
