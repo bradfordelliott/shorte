bf567a23cd1e278a7e688f0d7bf8797152b3faad
Brad Elliott <belliott@inphi.com>
2015-11-10 13:59:45 -0500

    Some fixes to make the linux install easier

--- src/3rdparty/clang/includes/stdint.h
+++ src/3rdparty/clang/includes/stdint.h
@@ -2,16 +2,6 @@
 #define _STDINT_H
 #include <stddef.h>
 
-/* 7.18.1.1  Exact-width integer types */
-typedef signed char int8_t;
-typedef unsigned char   uint8_t;
-typedef short  int16_t;
-typedef unsigned short  uint16_t;
-typedef int  int32_t;
-typedef unsigned   uint32_t;
-typedef long long  int64_t;
-typedef unsigned long long   uint64_t;
-
 /* 7.18.1.5  Greatest-width integer types */
 typedef long long  intmax_t;
 typedef unsigned long long   uintmax_t;
