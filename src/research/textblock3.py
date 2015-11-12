6dc4bfb59ef5947d823d439af3cc70e7d4e5c7da
Brad Elliott <belliott@inphi.com>
2015-11-11 21:43:53 -0500

    Added support for quoteblocks in the new textblock parser

--- src/research/textblock3.py
+++ src/research/textblock3.py
@@ -4,38 +4,7 @@ class span_t(object):
     def __init__(self):
         self.children = []
         self.source = ""
-    def __str__(self, prefix=""):
-        output =  "%s%s\n" % (prefix, type(self).__name__)
-        output += "%s---------\n" % prefix
-        lines = self.source.split("\n")
-        for line in lines:
-            output += "%s%s\n" % (prefix + "   >", line)
-
-        for child in self.children:
-            output += child.__str__(prefix + "   >")
-
-        return output
-
-class codeblock_t(span_t):
-    def __init__(self, language):
-        span_t.__init__(self)
-        self.language = language
-
-class listblock_t(span_t):
-    def __init__(self):
-        span_t.__init__(self)
-
-class textspan_t(span_t):
-    def __init__(self):
-        span_t.__init__(self)
-
-class textblock_t(object):
-
-    def __init__(self, source):
-        self.source = source
-        self.children = []
-        self.parse(source)
-
+    
     def parse(self, source):
 
         i = 0
@@ -45,6 +14,7 @@ class textblock_t(object):
         STATE_BACKTICKS = 1
         STATE_LIST = 2
         STATE_ORDERED_LIST = 3
+        STATE_QUOTE_BLOCK = 4
 
         states = []
         states.append(STATE_NORMAL)
@@ -74,7 +44,7 @@ class textblock_t(object):
                     else:
                         span += data[i]
                         i += 1
-                
+
                 # If the line starts with a number followed by . and then a space then it
                 # must be the start of an ordered list
                 elif((i == 0 or source[i-1] == "\n") and (i < len(source)-1) and ((source[i].isdigit()) and source[i+1:i+3] == ". ")):
@@ -82,6 +52,13 @@ class textblock_t(object):
                     span = listblock_t()
                     span.source += source[i]
                     states.append(STATE_ORDERED_LIST)
+                    i += 1
+
+                elif((i == 0 or source[i-1] == "\n") and (source[i] == ">")):
+                    parent.children.append(span)
+                    span = quoteblock_t()
+                    span.source += source[i]
+                    states.append(STATE_QUOTE_BLOCK)
                     i += 1
 
                 elif(source[i:i+3] == "```"):
@@ -117,22 +94,7 @@ class textblock_t(object):
                     i += 1
 
             elif(state == STATE_LIST):
-                if(source[i:i+3] == "```"):
-                    parent.children.append(span)
-                    states.append(STATE_BACKTICKS)
-                    i += 3
-                    
-                    # Search forward till we find the end of line. Everything
-                    # up till then is the language specifier
-                    language = ""
-                    while(i < end and source[i] != "\n"):
-                        language += source[i]
-                        i += 1
-
-                    parents.append(span)
-                    span = codeblock_t(language)
-
-                elif(source[i] == "\n" and (i > len(source)-2 or source[i+1] == "\n")):
+                if(source[i] == "\n" and (i > len(source)-2 or source[i+1] == "\n")):
                     span.source += source[i]
                     i += 2
                     parent.children.append(span)
@@ -146,20 +108,7 @@ class textblock_t(object):
                     i += 1
             
             elif(state == STATE_ORDERED_LIST):
-                if(source[i:i+3] == "```"):
-                    parent.children.append(span)
-                    states.append(STATE_BACKTICKS)
-                    i += 3
-                    
-                    # Search forward till we find the end of line. Everything
-                    # up till then is the language specifier
-                    language = ""
-                    while(i < end and source[i] != "\n"):
-                        language += source[i]
-                        i += 1
-                    parents.append(span)
-                    span = codeblock_t(language)
-                elif(source[i] == "\n" and (i > len(source)-2 or source[i+1] == "\n")):
+                if(source[i] == "\n" and (i > len(source)-2 or source[i+1] == "\n")):
                     span.source += source[i]
                     i += 2
                     parent.children.append(span)
@@ -173,13 +122,65 @@ class textblock_t(object):
                     span.source += source[i]
                     i += 1
 
+            elif(state == STATE_QUOTE_BLOCK):
+                if(source[i-1] == "\n" and source[i] != ">"):
+                    span.source += source[i]
+                    i += 1
+                    parent.children.append(span)
+                    states.pop()
+
+                    if(len(parents) > 1):
+                        span = parents.pop()
+                    else:
+                        span = textspan_t()
+                else:
+                    span.source += source[i]
+                    i += 1
+
+
             else:
                 print("Invalid state encountered!");
                 sys.exit(-1)
 
         if(len(span.source) != 0):
             parent.children.append(span)
+    def __str__(self, prefix=""):
+        output =  "%s%s\n" % (prefix, type(self).__name__)
+        output += "%s---------\n" % prefix
+        lines = self.source.split("\n")
+        for line in lines:
+            output += "%s%s\n" % (prefix + "   >", line)
 
+        for child in self.children:
+            output += child.__str__(prefix + "   >")
+
+        return output
+
+class quoteblock_t(span_t):
+    def __init__(self):
+        span_t.__init__(self)
+
+class codeblock_t(span_t):
+    def __init__(self, language):
+        span_t.__init__(self)
+        self.language = language
+
+class listblock_t(span_t):
+    def __init__(self):
+        span_t.__init__(self)
+
+class textspan_t(span_t):
+    def __init__(self):
+        span_t.__init__(self)
+
+class textblock_t(span_t):
+
+    def __init__(self, source):
+        span_t.__init__(self)
+
+        self.source = source
+        self.children = []
+        self.parse(source)
 
     def __str__(self, prefix=""):
         output =  "%sTextblock\n" % prefix
@@ -246,6 +247,13 @@ This is another paragraph here with a list in it:
         - five
 
 Followed by an indented code block
+
+    Indented block
+
+> This is a quote
+> This is also part of that quote
+
+This is a final paragraph
 """
     tb = textblock_t(text)
     print tb
